ea985c09330b124dc40f614788d633eb762535d2
aliadwnt <dewantoalia@gmail.com>
2025-06-15 02:38:20 +0700

    create dashboard public and update dataset

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
+from app.routes import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
-from flask import render_template
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def dashboard_public():
+    return render_template("dasboard-public.html")
